89c6871667a0b630044e6db8dd2226aeb5f125fd
DenizMurici <d.murici23@googlemail.com>
2016-05-25 01:51:14 +0200

    Suchleiste und Warenkorb

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
-document.addEventListener("DOMContentLoaded",init);
-
+window.addEventListener("load",init);
 
 // Loginpopup öffnen und wieder schließen mithilfe von flexibler Klasse zur ID
 // TODO: Passende CSS müssen erstellt werden.
 function init() {
-	if(document.getElementByID("popup").classname = popclose){ //falls die Klasse popclose ist
-		document.getElementByID("popup").addEventListener("click",openpop); //öffne openpop beim drücken auf link
+	/*if(document.getElementById("popup").classname == "popclose"){ //falls die Klasse popclose ist
+		document.getElementById("popup").addEventListener("click",openpop); //öffne openpop beim drücken auf link
 	}
 	else{
-		document.getElementByID("popup").addEventListener("click",closeform); //schließe openpop beim drücken auf link
-	}
+		document.getElementById("popup").addEventListener("click",closeform); //schließe openpop beim drücken auf link
+	}*/
+	document.getElementById("cartadd").addEventListener("click",warenkorb_add);
 	}
 
 function openpop(){
-	document.getElementById("popup").className = "popopen"
+	document.getElementById("popup").className = "popopen";
 }
 
 function closeform(){
-	document.getElementById("popup").className = "popclose"
+	document.getElementById("popup").className = "popclose";
 }
 
+function warenkorb_add(){
+	var element = document.getElementById("warenkorb");
+	element.innerHTML = "Warenkorb (1)";
+}
