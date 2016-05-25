8fb2d60d49233aad220695f124fd97af987d0ce4
Maximilian Pletschacher <maximilianpletschacher@MaximiliansMBP2.lan>
2016-05-25 05:00:53 +0200

    geile validations + suchergebnisse + textcontent + zahlungsauswahl

--- js/main.js
+++ js/main.js
@@ -15,6 +15,9 @@ function init() {
 		document.getElementById("regOpen2").addEventListener("click", function(){
 			popup("registrieren");
 		});
+		document.getElementById("search-box").addEventListener("submit", function(){
+			return checkSearch(event);
+		});
 }
 function popup(id){
 	if(document.getElementById(id).className == "popclose"){ //falls die Klasse popclose ist
@@ -23,4 +26,23 @@ function popup(id){
 	else{
 		document.getElementById(id).className = "popclose"
 	}
+}
+
+function checkSearch(event){
+	var eingabe = document.getElementById("search-box");
+	var i;
+	var sterne=false;
+	var warnung="";
+	var subReturn=true;
+    if(eingabe[0].value==""){
+    	warnung=warnung+"Bitte Suchwort angeben. ";
+    	subReturn=false;
+    }
+    if(warnung!=""){
+    	document.getElementById("sucheWarnung").innerHTML = ""+warnung+"";
+		document.getElementById("sucheWarnung").className = "popopen";
+		warnung="";
+		event.preventDefault();
+    }
+    return subReturn;
 }
